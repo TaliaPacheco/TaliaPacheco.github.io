1c5b953f48bcbdf03f2a75265bc2bb13e7f14fe3
Talia.dev <taliapacheco2002@gmail.com>
2026-01-06 18:37:13 -0300

    Configuraando variáveis de ambiente no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_GOOGLE_APPS_SCRIPT_URL': JSON.stringify(process.env.VITE_GOOGLE_APPS_SCRIPT_URL),
+    'import.meta.env.VITE_GOOGLE_APPS_SCRIPT_SECRET': JSON.stringify(process.env.VITE_GOOGLE_APPS_SCRIPT_SECRET),
+  },
 })
